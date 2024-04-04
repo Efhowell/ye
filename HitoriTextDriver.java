72fd1a64d4641d56acba669681da7de5d9c50ab4
Efhowell <52275540+Efhowell@users.noreply.github.com>
2024-04-04 16:36:45 -0500

    Update HitoriTextDriver with functionality for user interaction and puzzle solving.

--- HitoriTextDriver.java
+++ HitoriTextDriver.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
 
 public class HitoriTextDriver {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter the filename of the Hitori puzzle:");
+        String filename = scanner.nextLine();
+        
+        Hitori puzzle = new Hitori(filename);
+        
+        if (puzzle.solve()) {
+            System.out.println("Solved Puzzle:\n" + puzzle.toString());
+        } else {
+            System.out.println("No solution exists for this puzzle.");
+        }
+    }
 }
